63987a19199f6f9684570d0d7ec691dd8b51be59
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-05-15 06:37:53 +0100

    updates 7 user registeration

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const isValidUsername = require("../../helpers/regex/regex.helper");
+const { User } = require("../../models/user/User.model");
+const gravatar = require("gravatar");
+const bcrypt = require("bcrypt");
 
-router.get("/register", (req, res) => {
+/**
+ * @param {*POST} registration route
+* @route {/api/user/register }
+ */
+router.post("/register", async(req, res) => {
     // Destructuring form request
     const {name, username, email, password, confirmPassword} = req.body;
     
@@ -12,27 +19,63 @@ router.get("/register", (req, res) => {
     if(!name || !username || !email || !password){
         errors.push({msg: "Sorry all fields are required"});
     }
+    
+    // const valid_name = new RegExp(`/\^[A-Za-z]/\/\w{5, 29}$/`);
 
-    if(!isValidUsername(name)){
-        
-    } 
+    // if(!name.includes(valid_name(name))){
+    //     errors.push({msg: "Not a valid username"});
+    // } 
 
     // regex
     // const regex = ``
     // RegExp
     // const alid_name = `/\^a-z/\^0-9/`;
-    const valid_name = new RegExp(`/\^[A-Za-z]/\/\w{5, 29}$/`);
 
     // confirming password 
-    if(password !== confirmPassword && password.length < 8 && !password.includes(valid_name)) {
+    if(password !== confirmPassword && password.length < 8) {
         errors.push({msg: "Sorry password incorrect"});
     }
 
     if(errors.length > 0) {
-        res.json({msg: `${errors, req.body}`})
+        res.status(200).json({msg: `${errors, req.body}`})
     } else {
         // validation passed
-        
+        await User.findOne({ username: username })
+        .then(async(user) => {
+            if(user) {
+                errors.push({msg: `User is already registered`})
+                res.redirect("/api/user/register");
+                res.status(400).json({msg: `User is already registered`});
+            } else {
+                const profileAvatar = gravatar.url(req.body.email, {
+                    protocol: "https", // secure transport
+                    s: "200", //size
+                    r: "pg", //rating
+                    d: "mm" //default
+                })
+                const coverAvatar = gravatar.url(req.body.email, {
+                    protocol: "https", // secure transport
+                    s: "200", //size
+                    r: "pg", //rating
+                    d: "mm" //default
+                })
+                const newUser =  new User({
+                    name,
+                    username,
+                    email,
+                    password,
+                    profileAvatar,
+                    coverAvatar
+                })
+
+                try {
+                    const user = await newUser.save();
+                    res.status(200).json(user)
+                } catch(err) {
+                    console.log(err)
+                }
+            }
+        })
     }
 })
 
