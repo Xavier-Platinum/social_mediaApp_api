2846352e563f7329e15ed0873debeeb10a23b08f
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-05-15 05:15:35 +0100

    updates 5 regex expression

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -1,8 +1,34 @@
 const router = require("express").Router();
 
 router.get("/register", (req, res) => {
-    const {} = req.body;
+    // Destructuring form request
+    const {name, username, email, password, confirmPassword} = req.body;
     
+    // errors
+    const errors = [];
+
+    // validating form
+    if(!name || !username || !email || !password){
+        errors.push({msg: "Sorry all fields are required"});
+    }
+
+    // regex
+    // const regex = ``
+    // RegExp
+    // const alid_name = `/\^a-z/\^0-9/`;
+    const valid_name = new RegExp(`/\^[A-Za-z]/\/\w{5, 29}$/`)
+
+    // confirming password 
+    if(password !== confirmPassword && password.length < 8 && !password.includes(valid_name)) {
+        errors.push({msg: "Sorry password incorrect"});
+    }
+
+    if(errors.length > 0) {
+        res.json({msg: `${errors, req.body}`})
+    } else {
+        // validation passed
+        
+    }
 })
 
 module.exports =  router;
