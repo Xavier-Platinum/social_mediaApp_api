c5b1cb75ff5d9f1f204b1c5aadadbc4e1e9fb1a9
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-06-02 13:44:36 +0100

    added users _unfollow_route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -69,6 +69,25 @@ router.put("/:id/follow", async(req, res) => {
     }
 })
 // unfollow a user
+router.put("/:id/unfollow", async(req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            const user = await User.finById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: {followers: req.body.userId}});
+                await currentUser.updateOne({ $pull: {followings: req.body.userId}});
+                res.status(200).json("User has been unfollowed");
+            } else {
+                res.status(403).json("You don't follow this user")
+            }
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("Sorry you can't unfollow yourself");
+    }
+})
 
 router.get("/", (req, res) => {
     res.json("Hello users routes");
