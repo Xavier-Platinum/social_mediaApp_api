56c59bfd31d1fceecd71586f8773e870acceef68
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-06-02 14:01:14 +0100

    server updates

--- middlewares/routes.middleware.js
+++ middlewares/routes.middleware.js
@@ -1,6 +1,8 @@
 const usersRoutes =  require("../routes/users.routes");
 const authRoutes = require("../routes/auth/auth.routes");
+const postsRoutes = require("../routes/posts/posts.routes");
 module.exports = (app) => {
     app.use("/api/auth", authRoutes);
     app.use("/api/user", usersRoutes);
+    app.use("/api/posts", postsRoutes);
 }
