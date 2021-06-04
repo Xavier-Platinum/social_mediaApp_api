27f421ebe8b4eec06478b9d2aef3e8e8c4aa0f8a
Kwis Lawrence Francis <kwislawrencekwis@gmail.com>
2021-06-04 14:05:20 +0100

    added post creation route

--- routes/posts/posts.routes.js
+++ routes/posts/posts.routes.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
+const { Post } = require("../../models/posts/posts.model");
 
-// create post 
+// create post
+router.post("/", async(req, res) => {
+    // const {} = req.body;
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}) 
 // update post
 // delete posts
 // like post 
