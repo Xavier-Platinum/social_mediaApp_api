6fa7129decd8da4295724000211bcc97ca997a4f
Kwis Lawrence Francis <kwislawrencekwis@gmail.com>
2021-06-05 02:47:49 +0100

    updated post creation route

--- routes/posts/posts.routes.js
+++ routes/posts/posts.routes.js
@@ -7,11 +7,11 @@ router.post("/", async(req, res) => {
     const newPost = new Post(req.body);
     try {
         const savedPost = await newPost.save();
-        res.status(200).json(savedPost)
+        res.status(200).json(savedPost);
     } catch (err) {
         res.status(500).json(err);
     }
-}) 
+});
 // update post
 // delete posts
 // like post 
