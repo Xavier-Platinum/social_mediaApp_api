614f959bf58abd22ed762dc61db1b37e2ad4dc43
Kwis Lawrence Francis <kwislawrencekwis@gmail.com>
2021-06-05 13:00:39 +0100

    updated changes

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -77,6 +77,7 @@ router.post("/register", async(req, res) => {
                             // saving user
                             const user = await newUser.save();
                             res.status(200).json(user);
+                            console.log(user);
                         })
                     })
                     // const salt = await bcrypt.genSalt(10);
