49a5c6ebe6dd3c9e0260d45176f9e106121bc259
Kwis Lawrence Francis <kwislawrencekwis@gmail.com>
2021-06-05 02:54:05 +0100

    added post update route

--- routes/posts/posts.routes.js
+++ routes/posts/posts.routes.js
@@ -13,6 +13,19 @@ router.post("/", async(req, res) => {
     }
 });
 // update post
+router.put("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await Post.updateOne({$set: req.body});
+            res.status(200).json("Post has been updated");
+        } else {
+            res.status(403).json("You can only update your post")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 // delete posts
 // like post 
 // get a post 
