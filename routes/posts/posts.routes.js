853589e884ec2341b09b2de2c1326e8cc22755ad
Kwis Lawrence Francis <kwislawrencekwis@gmail.com>
2021-06-05 12:48:51 +0100

    updated ppost routes

--- routes/posts/posts.routes.js
+++ routes/posts/posts.routes.js
@@ -67,7 +67,7 @@ router.get("/:id", async(req, res) => {
 })
 // get timeline post
 router.get("/timeline/all", async(req, res) => {
-    let postArray = [];
+    // let postArray = [];
     try {
         const currentUser =  await User.findById(req.body.userId);
         const userPosts = await Post.find({ userId: currentUser._id });
